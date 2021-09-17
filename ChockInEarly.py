aa4171d6c0e64ee10b564c6c6ba7f4ab5e7286f4
Mashiro2000 <40681822+Mashiro2000@users.noreply.github.com>
2021-09-17 20:47:04 +0800

    Update ChockInEarly.py

--- ChockInEarly.py
+++ ChockInEarly.py
@@ -13,6 +13,7 @@ new Env('欢太早睡打卡');
 import os
 import sys
 import time
+import json
 import random
 import logging
 
@@ -102,6 +103,16 @@ class CheckInEarly:
             self.early()
         logger.info('*' * 40 + '\n')
 
+# 格式化设备信息Json
+# 由于青龙的特殊性,把CK中的 app_param 转换未非正常格式，故需要此函数
+def transform(string):
+    dic2 = {}
+    dic1 = eval(string)
+    for i in dic1['app_param'][1:-1].split(','):
+        dic2[i.split(':')[0]] = i.split(':')[-1]
+    dic1['CK'] = dic1['CK'] + f";app_param={json.dumps(dic2,ensure_ascii=False)}"
+    return dic1
+
 # 读取青龙CK
 def getEnv(key):
     lists = []
@@ -111,7 +122,7 @@ def getEnv(key):
         logger.info("青龙面板环境变量 TH_COOKIE 不存在！")
     else:
         for each in variable.split('&'):
-            lists.append(eval(each))
+            lists.append(transform(each))
     return lists
 
 if __name__ == '__main__':
