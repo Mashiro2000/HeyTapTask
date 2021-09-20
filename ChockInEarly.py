dc1ed458460d28292a88655e4df1025fbd5ad02b
Mashiro2000 <40681822+Mashiro2000@users.noreply.github.com>
2021-09-20 21:37:44 +0800

    Update ChockInEarly.py

--- ChockInEarly.py
+++ ChockInEarly.py
@@ -127,7 +127,7 @@ def getEnv(key):
 
 if __name__ == '__main__':
     for each in getEnv('HT_COOKIE'):
-        if each['CK'] != "" and each['UA'] != "":
+        if all(each.values()):
             checkInEarly = CheckInEarly(each)
             for count in range(3):
                 try:
