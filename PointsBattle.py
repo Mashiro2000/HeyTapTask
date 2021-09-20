d5c1583017bb642b06a3ba127feacd7998f15b20
Mashiro2000 <40681822+Mashiro2000@users.noreply.github.com>
2021-09-20 21:38:07 +0800

    Update PointsBattle.py

--- PointsBattle.py
+++ PointsBattle.py
@@ -226,7 +226,7 @@ def getEnv(key):
 
 if __name__ == '__main__':
     for each in getEnv('HT_COOKIE'):
-        if each['CK'] != "" and each['UA'] != "":
+        if all(each.values()):
             pointsBattle = PointsBattle(each)
             for count in range(3):
                 try:
