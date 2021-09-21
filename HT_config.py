f1053f042343b32bcac60beb267653d72c3058b0
Mashiro2000 <40681822+Mashiro2000@users.noreply.github.com>
2021-09-21 09:19:44 +0800

    Create HT_config.py

---
+++ HT_config.py
@@ -0,0 +1,27 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/12
+# @Author  : 2984922017@qq.com
+# @File    : HT_config.py
+# @Software: PyCharm
+
+## 格式样本
+# {
+#     'user':'',        # 备注,必要
+#     'CK':'',          # Cookie,必要
+#     'UA':''           # User-Agent,必要
+# },
+
+## 账号管理
+accounts = [
+    {
+        'user':'',
+        'CK':'',
+        'UA':''
+    },
+    {
+        'user':'',
+        'CK':'',
+        'UA':''
+    }
+]
