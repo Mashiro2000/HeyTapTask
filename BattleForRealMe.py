a94a283c761c5d1bb222659180b4d0a8654ca3ef
Mashiro2000 <40681822+Mashiro2000@users.noreply.github.com>
2021-10-12 17:56:54 +0800

    更新随机延迟

--- BattleForRealMe.py
+++ BattleForRealMe.py
@@ -150,7 +150,7 @@ class BattleForRealMe:
             self.receiveAward(dic)
         else:
             notify(f"[{dic['title']}]\t{response['msg']}")
-            time.sleep(random.randint(3,5))
+        time.sleep(random.randint(3,5))
 
     def getBattleList(self):
         aid = 1582  # 抓包结果为固定值:1582
