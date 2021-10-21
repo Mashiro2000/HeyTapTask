ecf55c8ff5b0ad7235f0a0f9419e34e8d73d37fc
Mashiro2000 <40681822+Mashiro2000@users.noreply.github.com>
2021-10-21 20:40:08 +0800

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2021/9/16
 # @Author  : MashiroF
-# @File    : BattleForRealMe.py
+# @File    : index.py
 # @Software: PyCharm
 import TaskCenter
 
@@ -22,13 +22,13 @@ def main_handler(event, context):
         print("非定时器触发")
         print(event)
 
-if __name__ == '__main__':
-    #main_handler(None, None)
-    event = {
-        "Type": "Timer",
-        "TriggerName": "TaskCenter",
-        "Time": "2019-02-21T11:49:00Z",
-        "Message": "user define msg body"
-    }
-    # 设置多个定时器触发，定时器名称为对应模块名称
-    main_handler(event, None)
+# if __name__ == '__main__':
+#main_handler(None, None)
+event = {
+    "Type": "Timer",
+    "TriggerName": "TaskCenter",
+    "Time": "2019-02-21T11:49:00Z",
+    "Message": "user define msg body"
+}
+# 设置多个定时器触发，定时器名称为对应模块名称
+main_handler(event, None)
