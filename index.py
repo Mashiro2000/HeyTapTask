cbdb9ad1a42ca4812159774ac00818259d98edff
Mashiro2000 <40681822+Mashiro2000@users.noreply.github.com>
2021-10-27 23:32:27 +0800

    更新云函数多合一脚本

--- index.py
+++ index.py
@@ -1,34 +1,16 @@
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# @Time    : 2021/9/16
+# @Time    : 2021/10/27
 # @Author  : MashiroF
 # @File    : index.py
 # @Software: PyCharm
-import TaskCenter
-
-cron = {
-    'TaskCenter': '25 8,13 * * *',
-}
-
+from importlib import import_module
 
 def run(taskName: str):
-    globals().get(taskName).main_handler(None, None)
+    taskName = import_module(taskName)
+    taskName.main_handler(None, None)
 
 
 def main_handler(event, context):
-    if 'TriggerName' in event:
-        run(event['TaskCenter'])
-    else:
-        print("非定时器触发")
-        print(event)
-
-# if __name__ == '__main__':
-#main_handler(None, None)
-event = {
-    "Type": "Timer",
-    "TriggerName": "TaskCenter",
-    "Time": "2019-02-21T11:49:00Z",
-    "Message": "user define msg body"
-}
-# 设置多个定时器触发，定时器名称为对应模块名称
-main_handler(event, None)
+    task = event['TriggerName'].split('\r\n')
+    run(task)
