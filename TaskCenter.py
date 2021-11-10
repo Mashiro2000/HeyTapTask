f59f1f0dc1e8daa9869ddd96bab5a48dc1b89c32
Mashiro2000 <40681822+Mashiro2000@users.noreply.github.com>
2021-11-10 16:39:52 +0800

    Update TaskCenter.py

--- TaskCenter.py
+++ TaskCenter.py
@@ -397,6 +397,26 @@ class TaskCenter:
             self.runEarnPoint()         # 赚积分
             # self.runViewPush()          # 浏览推送任务(已下架)
 
+    # 获取积分数量(只找到这个，找不到昨天积分数据)
+    def getIntegral(self):
+        url = 'https://store.oppo.com/cn/oapi/credits/web/credits/show'
+        headers = {
+            'Host': 'store.oppo.com',
+            'Connection': 'keep-alive',
+            'source_type': '501',
+            'clientPackage': 'com.oppo.store',
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,en-US;q=0.9',
+            'X-Requested-With': 'com.oppo.store',
+            'Referer': 'https://store.oppo.com/cn/app/taskCenter/index?us=gerenzhongxin&um=hudongleyuan&uc=renwuzhongxin'
+        }
+        response = self.sess.get(url=url,headers=headers).json()
+        if response['code'] == 200:
+            return f"{self.dic['user']}\t总积分:{response['data']['userCredits']}"
+        else:
+            return f"{self.dic['user']}\t错误原因:{response}"
+
     # 执行欢太商城实例对象
     def start(self):
         self.sess.headers.update({
@@ -408,6 +428,7 @@ class TaskCenter:
         if self.login() == True:
             if self.getTaskList() == True:              # 获取任务中心数据，判断CK是否正确(登录可能成功，但无法跑任务)
                 self.runTaskCenter()                    # 运行任务中心
+                notify(self.getintegral())
             notify('*' * 40 + '\n')
 
 # 检测CK是否存在必备参数
