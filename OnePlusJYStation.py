3dc2da0194a8972953e1cfbb740ffdeec8207f55
Mashiro2000 <40681822+Mashiro2000@users.noreply.github.com>
2021-11-14 21:21:34 +0800

    Update OnePlusJYStation.py

--- OnePlusJYStation.py
+++ OnePlusJYStation.py
@@ -287,7 +287,7 @@ def checkHT(dic):
 def main_handler(event, context):
     global lists
     for each in lists:
-        if all(each.values()):
+        if each['CK']!='' and each['UA'] != '':
             if checkHT(each):
                 onePlusJYStation = OnePlusJYStation(each)
                 for count in range(3):
