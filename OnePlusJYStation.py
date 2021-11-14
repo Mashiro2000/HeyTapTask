a3c076a9f4097fc47b592d46df24136da47bfb2b
Mashiro2000 <40681822+Mashiro2000@users.noreply.github.com>
2021-11-14 17:20:27 +0800

    Update OnePlusJYStation.py

--- OnePlusJYStation.py
+++ OnePlusJYStation.py
@@ -302,6 +302,8 @@ def main_handler(event, context):
                 else:
                     notify(f"账号: {onePlusJYStation.dic['user']}\n状态: 取消登录\n原因: 多次登录失败")
                     break
+        else:
+            notify(f"账号: {onePlusJYStation.dic['user']}\n状态: 取消登录\n原因: json数据不齐全")
     if not os.path.basename(__file__).split('_')[-1][:-3] in notifyBlackList:
         send('一加加油站',allMess)
 
