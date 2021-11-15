7b9d6a62e74c6d9a1c23ee7e29fc9c8f6ab77fbc
Mashiro2000 <40681822+Mashiro2000@users.noreply.github.com>
2021-11-15 23:43:40 +0800

    Update BattleForHeyTap.py

--- BattleForHeyTap.py
+++ BattleForHeyTap.py
@@ -279,7 +279,7 @@ def main_handler(event, context):
                 else:
                     notify(f"账号: {battleForHeyTap.dic['user']}\n状态: 取消登录\n原因: 多次登录失败")
                     break
-        elif all(each.values()):
+        elif not all(each.values()):
             notify("账号:空账户\t状态:跳过")
         else:
             notify(f"账号: {each['user']}\n状态: 取消登录\n原因: json数据不齐全")
