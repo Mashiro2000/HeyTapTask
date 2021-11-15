4a13af5a6a643c3f8d8d4d5024a09cf074940cb9
Mashiro2000 <40681822+Mashiro2000@users.noreply.github.com>
2021-11-15 23:42:22 +0800

    Update TimingCash.py

--- TimingCash.py
+++ TimingCash.py
@@ -181,7 +181,7 @@ def main_handler(event, context):
                     notify(f"账号: {timingCash.dic['user']}\n状态: 取消登录\n原因: 多次登录失败")
                     break
         elif all(each.values()):
-            pass
+            notify("账号:空账户\t状态:跳过")
         else:
             notify(f"账号: {each['user']}\n状态: 取消登录\n原因: json数据不齐全")
     if not os.path.basename(__file__).split('_')[-1][:-3] in notifyBlackList:
